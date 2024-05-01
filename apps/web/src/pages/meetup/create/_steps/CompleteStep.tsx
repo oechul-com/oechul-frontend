da4f4a7ed534ade2cd43c449a7fa70d181fc38b3
ohprettyhak <haklee.googl@gmail.com>
2024-05-01 16:13:39 +0900

    feat: Guideline 컴포넌트 추가

--- apps/web/src/pages/meetup/create/_steps/CompleteStep.tsx
+++ apps/web/src/pages/meetup/create/_steps/CompleteStep.tsx
@@ -1,12 +1,11 @@
 import { rem, theme } from '@oechul/styles';
-import { Accordion, Input, Text } from '@oechul/ui';
+import { Input, Text } from '@oechul/ui';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import Guideline from '@/components/Guideline';
+import { COPY_MESSAGE } from '@/constants.ts';
 import {
-  AccordionContent,
-  AccordionHeader,
-  AccordionToggleContainer,
   CompleteContent,
   CreateCompleteContent,
   InviteCodeCopyButton,
@@ -22,12 +21,43 @@ interface CompleteStepProps {
   formData: MeetupForm;
 }
 
+const guidelines = [
+  {
+    emoji: '💁🏻‍♀️',
+    title: '초대 코드 사용법',
+    contents: [
+      '팀 코드를 저장하거나 캡쳐해서 팀원들에게 보내주세요!',
+      '초기에 설정된 팀 인원은 수정이 불가능해요. 추가하고 싶은 인원이 있으면 새롭게 팀을 만들어보세요!',
+    ],
+  },
+  {
+    emoji: '💘',
+    title: '매칭은 어떤 방식으로 이루어지나요?',
+    contents: [
+      '매칭리스트 → 원하는 팀 선택 → 매칭 신청 → 상대방이 매칭 수락 시 매칭 성공',
+      '상대방의 매칭 요청 → 매칭 수락 시 매칭 성공',
+    ],
+  },
+  {
+    emoji: '💘',
+    title: '매칭이 성공하면 어떻게 되나요?',
+    contents: [
+      '매칭 성공 시 각 팀에게 해당 페이지가 전송됩니다. 대표 연락처를 통해 매칭 날짜와 장소를 정해주세요!\n매칭 성공 시 팀 삭제 및 매칭 취소가 불가능해요.',
+    ],
+  },
+];
+
 const CompleteStep = ({ formData }: CompleteStepProps) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (!validateFormStep(formData, 'complete'))
       navigate('/meetup/create', { replace: true });
   }, [formData, navigate]);
+
+  const handleCopyClick = (inviteCode: string) => {
+    if (copyTextToClipboard(inviteCode)) alert(COPY_MESSAGE.SUCCESS);
+    else alert(COPY_MESSAGE.FAILURE);
+  };
 
   return (
     <>
@@ -49,72 +79,18 @@ const CompleteStep = ({ formData }: CompleteStepProps) => {
               readOnly={true}
             />
             <InviteCodeCopyButton
-              onClick={() =>
-                copyTextToClipboard('13D2-4WE2-4KD2-1E22D')
-                  ? alert('복사되었습니다.')
-                  : alert('복사에 실패했습니다. 다시 시도해주세요.')
-              }
+              onClick={() => handleCopyClick('13D2-4WE2-4KD2-1E22D')}
             >
               복사
             </InviteCodeCopyButton>
           </InviteCodeInputContainer>
 
-          <Accordion>
-            <Accordion.Content>
-              <AccordionHeader>
-                <span>💁🏻‍♀</span>
-                <span>초대 코드 사용법</span>
-                <Accordion.Toggle as={AccordionToggleContainer} />
-              </AccordionHeader>
-              <Accordion.Collapse divider={true}>
-                <AccordionContent>
-                  <li>팀 코드를 저장하거나 캡쳐해서 팀원들에게 보내주세요!</li>
-                  <li>
-                    초기에 설정된 팀 인원은 수정이 불가능해요. 추가하고 싶은
-                    인원이 있으면 새롭게 팀을 만들어보세요!
-                  </li>
-                </AccordionContent>
-              </Accordion.Collapse>
-            </Accordion.Content>
-          </Accordion>
-
-          <Accordion>
-            <Accordion.Content>
-              <AccordionHeader>
-                <span>💘</span>
-                <span>매칭은 어떤 방식으로 이루어지나요?</span>
-                <Accordion.Toggle as={AccordionToggleContainer} />
-              </AccordionHeader>
-              <Accordion.Collapse divider={true}>
-                <AccordionContent>
-                  <li>
-                    매칭리스트 → 원하는 팀 선택 → 매칭 신청 → 상대방이 매칭 수락
-                    시 매칭 성공
-                  </li>
-                  <li>상대방의 매칭 요청 → 매칭 수락 시 매칭 성공</li>
-                </AccordionContent>
-              </Accordion.Collapse>
-            </Accordion.Content>
-          </Accordion>
-
-          <Accordion>
-            <Accordion.Content>
-              <AccordionHeader>
-                <span>💘</span>
-                <span>매칭이 성공하면 어떻게 되나요?</span>
-                <Accordion.Toggle as={AccordionToggleContainer} />
-              </AccordionHeader>
-              <Accordion.Collapse divider={true}>
-                <AccordionContent>
-                  매칭 성공 시 각 팀에게 해당 페이지가 전송됩니다. 대표 연락처를
-                  통해 매칭 날짜와 장소를 정해주세요! 매칭 성공 시 팀 삭제 및
-                  매칭 취소가 불가능해요.
-                </AccordionContent>
-              </Accordion.Collapse>
-            </Accordion.Content>
-          </Accordion>
+          {guidelines.map((data, index) => (
+            <Guideline key={index} {...data} />
+          ))}
         </CompleteContent>
       </CreateCompleteContent>
+
       <SaveButtonContainer>
         <SaveButton
           variant="accent"
