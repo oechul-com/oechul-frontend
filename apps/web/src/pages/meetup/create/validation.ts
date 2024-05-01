4420f53c2bde6b6c07551db9e9d7df766e9aaa41
ohprettyhak <haklee.googl@gmail.com>
2024-05-01 15:38:44 +0900

    chore: form validation 코드 정리

--- apps/web/src/pages/meetup/create/validation.ts
+++ apps/web/src/pages/meetup/create/validation.ts
@@ -9,24 +9,19 @@ const requiredFieldsByStep: Record<
   complete: ['groupType', 'teamName', 'days', 'contactInfo'],
 };
 
-const hasRequiredFields = (
-  formData: MeetupForm,
-  fields: (keyof MeetupForm | 'contactInfo')[],
-): boolean => {
-  return fields.every(field => {
-    if (field === 'days') {
-      return formData.days.length > 0;
-    }
-    if (field === 'contactInfo') {
-      return formData.phoneNumber || formData.kakaoTalk || formData.instagram;
-    }
-    return Boolean(formData[field]);
-  });
-};
-
 export const validateFormStep = (
   formData: MeetupForm,
   step: 'name' | 'contact' | 'complete',
 ): boolean => {
-  return hasRequiredFields(formData, requiredFieldsByStep[step]);
+  const fields: (keyof MeetupForm | 'contactInfo')[] =
+    requiredFieldsByStep[step];
+
+  return fields.every(field => {
+    if (field === 'days') return formData.days.length > 0;
+
+    if (field === 'contactInfo')
+      return formData.phoneNumber || formData.kakaoTalk || formData.instagram;
+
+    return Boolean(formData[field]);
+  });
 };
