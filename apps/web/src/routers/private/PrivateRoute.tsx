67b3c266cf0347345aef34b247ca440197d8bb3e
ohprettyhak <haklee.googl@gmail.com>
2024-05-14 11:18:35 +0900

    feat: Spinner 컴포넌트 및 로딩창 추가

--- apps/web/src/routers/private/PrivateRoute.tsx
+++ apps/web/src/routers/private/PrivateRoute.tsx
@@ -3,13 +3,14 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { signedInState, authLoadingState } from '@/atoms/authState.ts';
+import Loading from '@/components/Loading';
 
 const PrivateRoute = (): ReactElement => {
   const isSignedIn = useRecoilValue(signedInState);
   const isLoading = useRecoilValue(authLoadingState);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   return isSignedIn ? <Outlet /> : <Navigate to="/" />;
